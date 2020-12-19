022dd9d543bc78516067ec36c51df0162ad009d0
JamesDoe <jamesdoejr@gmail.com>
2020-12-18 23:23:25 -0500

    Update farmapp.js

--- farmapp.js
+++ farmapp.js
@@ -4,4 +4,11 @@ const Critical_Tracking_Events = {
 2: 'transforming', 
 3: 'creating', 
 4: 'shipping'
+},
+Key_Data_Elements = {
+0: ''
+}
+
+class Foo(obj) {
+this.traceability_lot_code = obj.traceability_lot_code
 }
