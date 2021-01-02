37cb348234969141b0c55252e6c9e05c931561bf
JamesDoe <jamesdoejr@gmail.com>
2021-01-02 15:12:52 -0500

    Update farmapp.js

--- farmapp.js
+++ farmapp.js
@@ -12,3 +12,21 @@ Key_Data_Elements = {
 class Foo(obj) {
 this.traceability_lot_code = obj.traceability_lot_code
 }
+
+const farm_object = {
+supplier-seller_name: "",
+supplier-seller_contact_information: "",
+supplier_BID: "",
+customer_name: "",
+customer_BID: "",
+product_description: "", (Brand name if applicable, including variety)
+lot_number or other batch identifier of the good
+ Quantity and packaging information
+ Purchasing or receiving date
+ Address from where the good was purchased/delivered
+ Name and contact information of the transporter
+ Any other information deemed appropriate by the Competent Authority
+}
+class Farm (obj) {
+
+}
